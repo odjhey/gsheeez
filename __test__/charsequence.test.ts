44cd42b403542396f46b5b06fbfce10905727ebf
Odee <odjhey@gmail.com>
2020-04-03 17:24:53 +0800

    refactor char utils and add generate range

--- __test__/charsequence.test.ts
+++ __test__/charsequence.test.ts
@@ -1,62 +1,8 @@
-const nextChar = (curr) => {
-  const currNoCase = curr.toUpperCase(curr)
-
-  const max = 'Z'.charCodeAt(0) //90
-  const min = 'A'.charCodeAt(0) //65
-
-  const arr = currNoCase.split('')
-
-  const getNextNum = (
-    num,
-  ): { curr: number; next: number; overflow: boolean } => {
-    if (num < max) {
-      return {
-        curr: num,
-        next: num + 1,
-        overflow: false,
-      }
-    } else {
-      return {
-        curr: num,
-        next: min,
-        overflow: true,
-      }
-    }
-  }
-
-  const nextArrObj = arr.map((item) => {
-    const charCode = item.charCodeAt(0)
-    const nextCharCode = getNextNum(charCode)
-    return nextCharCode
-  })
-
-  const nextToken = nextArrObj
-    .reverse()
-    .reduce((accu, item, idx, arr) => {
-      if (idx === 0) {
-        accu.push(item.next)
-      } else {
-        if (arr[idx - 1].overflow) {
-          //if previous is overflow
-          accu.push(item.next)
-        } else {
-          accu.push(item.curr)
-        }
-      }
-
-      // last token
-      if (idx + 1 === arr.length && item.overflow) {
-        accu.push(min)
-      }
-      return accu
-    }, [])
-    .reverse()
-    .map((item) => String.fromCharCode(item))
-
-  return nextToken.join('')
-}
+import charUtils from '../src/core/charUtils'
 
 describe('char sequence A-ZZZ', () => {
+  const { nextChar } = charUtils
+
   it('should be able to get next char after a', () => {
     const given = 'A'
     const next = nextChar(given)
@@ -104,5 +50,72 @@ describe('char sequence A-ZZZ', () => {
     const next = nextChar(given)
 
     expect(next).toBe('AEA')
+  })
+})
+
+describe('char compare', () => {
+  const { compare } = charUtils
+  it('should be able to determine A comes before B', () => {
+    const res = compare('A', 'B')
+    expect(res).toBeLessThan(0)
+  })
+  it('should be able to determine B comes after A', () => {
+    const res = compare('B', 'A')
+    expect(res).toBeGreaterThan(0)
+  })
+  it('should be able to determine equality', () => {
+    const isGreater = compare('A', 'A')
+    expect(isGreater).toBe(0)
+  })
+
+  it('should be able to determine AA comes before BB -- multi', () => {
+    const res = compare('AA', 'B')
+    expect(res).toBeGreaterThan(0)
+  })
+  it('should be able to determine BB comes after AA -- multi', () => {
+    const res = compare('B', 'AA')
+    expect(res).toBeLessThan(0)
+  })
+  it('should be able to determine equality -- multi', () => {
+    const isGreater = compare('AA', 'AA')
+    expect(isGreater).toBe(0)
+  })
+
+  it('should be able to determine AA comes before BB -- multi > 2', () => {
+    const res = compare('ABA', 'ABC')
+    expect(res).toBeLessThan(0)
+  })
+  it('should be able to determine BB comes after AA -- multi > 2', () => {
+    const res = compare('ABA', 'AAA')
+    expect(res).toBeGreaterThan(0)
+  })
+  it('should be able to determine equality -- multi > 2', () => {
+    const isGreater = compare('ZZZ', 'ZZZ')
+    expect(isGreater).toBe(0)
+  })
+})
+
+describe('range create', () => {
+  const { generateFromRange } = charUtils
+
+  it('should be able to generate a simple range', () => {
+    const range = generateFromRange('A', 'C')
+    expect(range).toEqual(['A', 'B', 'C'])
+  })
+
+  it('should be able to generate a simple long range', () => {
+    const range = generateFromRange('A', 'G')
+    expect(range).toEqual([
+      'A', 'B', 'C',
+      'D', 'E', 'F',
+      'G'
+    ])
+  })
+  it('should be able to generate a simple long range grow - muli letter', () => {
+    const range = generateFromRange('Z', 'AD')
+    expect(range).toEqual([
+      'Z', 'AA', 'AB',
+      'AC', 'AD'
+    ])
   })
 })
---
+++ src/core/charUtils.ts
@@ -0,0 +1,89 @@
+const charUtils = (() => {
+  const nextChar = (curr) => {
+    const currNoCase = curr.toUpperCase(curr)
+
+    const max = 'Z'.charCodeAt(0) //90
+    const min = 'A'.charCodeAt(0) //65
+
+    const arr = currNoCase.split('')
+
+    const getNextNum = (
+      num,
+    ): { curr: number; next: number; overflow: boolean } => {
+      if (num < max) {
+        return {
+          curr: num,
+          next: num + 1,
+          overflow: false,
+        }
+      } else {
+        return {
+          curr: num,
+          next: min,
+          overflow: true,
+        }
+      }
+    }
+
+    const nextArrObj = arr.map((item) => {
+      const charCode = item.charCodeAt(0)
+      const nextCharCode = getNextNum(charCode)
+      return nextCharCode
+    })
+
+    const nextToken = nextArrObj
+      .reverse()
+      .reduce((accu, item, idx, arr) => {
+        if (idx === 0) {
+          accu.push(item.next)
+        } else {
+          if (arr[idx - 1].overflow) {
+            //if previous is overflow
+            accu.push(item.next)
+          } else {
+            accu.push(item.curr)
+          }
+        }
+
+        // last token
+        if (idx + 1 === arr.length && item.overflow) {
+          accu.push(min)
+        }
+        return accu
+      }, [])
+      .reverse()
+      .map((item) => String.fromCharCode(item))
+
+    return nextToken.join('')
+  }
+
+  const compare = (a, b) => {
+    /**
+     * Negative when the referenceStr occurs before compareStr
+     * Positive when the referenceStr occurs after compareStr
+     * Returns 0 if they are equivalent
+     * DO NOT rely on exact return values of -1 or 1.
+     * NOTE: Negative and positive integer results vary between browsers
+     * (as well as between browser versions) because
+     * the W3C specification only mandates negative and positive values.
+     * Some browsers may return -2 or 2 or even some
+     * other negative or positive value.
+     */
+
+    return a.length - b.length || a.localeCompare(b, 'en')
+  }
+
+  const generateFromRange = (from: string, to: string): Array<string> => {
+    const range = []
+    let token = from
+    while (compare(token, to) <= 0) {
+      range.push(token)
+      token = nextChar(token)
+    }
+    return range
+  }
+
+  return { nextChar, compare, generateFromRange }
+})()
+
+export default charUtils
